8f6b284cd704b29eb449d44da66bc00d568c7920
Constanza Sosa <constanza.sosa.g@gmail.com>
2019-07-29 11:20:38 +0200

    Other solutions

--- js/main.js
+++ js/main.js
@@ -1,37 +1,56 @@
-const counter = document.querySelector('.counter');
-const input = document.querySelector('.input__text');
-const button = document.querySelector('.input__button');
-const clue = document.querySelector('.input__clue');
+//me ha faltado el use strict y la indentacion. 
+"use strict";
+
+const counter = document.querySelector(".counter");
+const input = document.querySelector(".input__text");
+const button = document.querySelector(".input__button");
+const clue = document.querySelector(".input__clue");
 const myRandomNumber = getRandomNumber(100);
 let howManyTimes = 0;
-const growingCounter = growCounter;
+counter.innerHTML = howManyTimes;
+// const growingCounter = growCounter; --> Esta no la he utilizado
 console.log(`Mi número aleatorio es ${myRandomNumber}`);
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-function growCounter(){
-return howManyTimes++;
+function growCounter() {
+  return howManyTimes++;
 }
+// esta la hubiera mejorado: 
+// function showCounter(num){
+//   counter.container.innerHTML = num;
+// }
 
-function playWithNumber(event){
+//Mejora: para no escribir siempre lo de clue.innerHTML: 
+// function showClue(text){
+//   clue.innerHTML = text;
+// }
+
+function playWithNumber(event) {
   let myInputNumber = parseInt(input.value);
-  console.log(`Mi número introducido es ${myInputNumber}`);
+  // console.log(`Mi número introducido es ${myInputNumber}`); sobra el console log
 
-  if (myInputNumber === myRandomNumber){
+  if (myInputNumber === myRandomNumber) {
+    //Acordarme de poner solo una comilla 
     clue.innerHTML = '¡HAS GANADO, CAMPEONA!';
-  }
-  else if (myInputNumber > myRandomNumber){
+  } else if (myInputNumber > myRandomNumber) {
+    // si queremos que cuente cuando fallamos:
+    // howManyTimes += 1;
+    // counter.innerHTML = howManyTimes;
     clue.innerHTML = 'Demasiado alto';
-  } 
-  else{
+  } else {
+    // si queremos que cuente cuando fallamos:
+    // howManyTimes += 1;
+    // counter.innerHTML = howManyTimes;
     clue.innerHTML = 'Demasiado bajo';
   }
-
+  
   counter.innerHTML = growingCounter();
-
+  // si queremos que cuente siempre:
+  // howManyTimes += 1;
+  // counter.innerHTML = howManyTimes;
 }
 
-
-button.addEventListener('click', playWithNumber);
+button.addEventListener("click", playWithNumber);
