5b6db7c79a76bc8948f2cbaa4c0b9f4280b64c0d
Constanza Sosa <constanza.sosa.g@gmail.com>
2019-08-03 22:18:10 +0200

    Try to add popup but still not working

--- js/main.js
+++ js/main.js
@@ -7,7 +7,18 @@ const clue = document.querySelector('.input__clue');
 const reset = document.querySelector('.play__button');
 const guess = document.querySelector('.guess__button');
 const guessText = document.querySelector('.guess_text');
+const label = document.querySelector('.input__label');
+const popup = document.querySelector('.popup');
+const nameInput = document.querySelector('.input__name');
 
+let name = nameInput.value
+
+if (name !== null){
+label.innerHTML = `Hola ${name}, introduce aquí tu número:`;
+} 
+else{
+  label.innerHTML = `Hola, introduce aquí tu número:`;
+}
 
 
 let counter = 0;
@@ -24,13 +35,18 @@ function playWithNumber(event){
   console.log(`Mi número introducido es ${myInputNumber}`);
   
   if (isNaN(myInputNumber) === true ){
-    clue.innerHTML = '¡Tienes que introducir un numero del 0 al 100 para jugar¡😃';
+    clue.innerHTML = '¡Tienes que introducir un numero del 0 al 100 para jugar! 😃';
   }
   else if (myInputNumber < 0 || myInputNumber > 100){
     clue.innerHTML = 'Tienes que introducir un número entre 0 y 100 --> No hagas trampa';
   }
   else if (myInputNumber === myRandomNumber){
-    clue.innerHTML = '¡HAS GANADO, CAMPEONA!';
+    if (counter === 1){
+      clue.innerHTML = `¡Enhorabuena, ${name}! </br> Lo has conseguido en ${counter} intento`;
+    } 
+    else {
+    clue.innerHTML = `¡Enhorabuena, ${name}! </br> Lo has conseguido en ${counter} intentos`;
+    }
   }
   else if (myInputNumber > myRandomNumber){
     clue.innerHTML = 'Demasiado alto';
@@ -40,7 +56,6 @@ function playWithNumber(event){
     clue.innerHTML = 'Demasiado bajo';
     counter ++;
   }
-
 
   counterDisplay.innerHTML = counter;
 
@@ -54,13 +69,25 @@ function playAgain(){
   counterDisplay.innerHTML = counter;
   clue.innerHTML = 'Escribe un número y dale a prueba';
   guessText.innerHTML = '';
+  name = prompt('¿Cómo te llamas?');
+  label.innerHTML = `Hola ${name}, introduce aquí tu número:`;
 }
 
 function seeNumber(){
   guessText.innerHTML = `El número es ${myRandomNumber}`;
+  clue.innerHTML = '¡Vuelve a empezar!';
 }
 
+function hidePopup(){
+  popup.classList.add('hidden');
+}
 
+nameInput.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    popup.click();
+  }
+});
+popup.addEventListener('change', hidePopup);
 button.addEventListener('click', playWithNumber);
 reset.addEventListener('click', playAgain);
 guess.addEventListener('click', seeNumber);
